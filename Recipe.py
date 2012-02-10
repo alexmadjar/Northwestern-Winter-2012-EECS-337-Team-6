76a44001e7b182000dd43d09959ce197776176c6
Nate Harner <nateharner01@gmail.com>
2012-02-09 22:58:48 -0600

    Added directions parsing from HTML

--- Recipe.py
+++ Recipe.py
@@ -5,6 +5,7 @@ import IngredientParser
 
 class Recipe:
     ingredients = []
+    directions = []
 
 class Ingredient:
         quantity = 0 #e.g. 1
@@ -22,10 +23,9 @@ def FetchRecipe(url):
      
     #Create the soup object from the HTML data
     soup = BeautifulSoup(html_data)
+    recipeFromURL = Recipe()
     
     ingredient_tags = soup.findAll('li',attrs={"class" : "plaincharacterwrap ingredient"})
-    
-    recipeFromURL = Recipe()
     ingredients = [tag.contents[0] for tag in ingredient_tags]
     
     #Format the ingredients to remove \r\n and leading spaces 
@@ -38,15 +38,29 @@ def FetchRecipe(url):
               re.sub(regex, '', ingredient_name)
             )
          )
+    
+    #Directions
+    directions_tags = soup.findAll('div', attrs={"class" : "directions"})
+    directions_tags = directions_tags[0]('span', attrs={"class" : "plaincharacterwrap break"})
+    directions = [tag.contents[0] for tag in directions_tags]
+    regex = '\\r\\n.[ ]+'
+    for step_in_directions in directions:
+        print step_in_directions
+        recipeFromURL.directions.append(
+              re.sub(regex, '', step_in_directions)
+            )
 
     return recipeFromURL
     
 
-def PrintRecipe(recipy):
-    print recipy.ingredients
+def PrintRecipe(recipe):
+    print recipe.ingredients
+    print recipe.directions
     return None
 
 
-def VegetarianVersion(recipy):
-    return recipy
+def VegetarianVersion(recipe):
+    #Version 1: Replace the meat ingredients with a random vegitarian substitute
+    
+    return recipe
 
