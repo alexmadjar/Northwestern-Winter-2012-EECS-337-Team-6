0f0d645cf5ad3c23d4be143984e7dea8f8bed1ca
Alex Madjar <madjara13@gmail.com>
2012-03-12 15:08:54 -0500

    candidate tags

--- twitter/Candidates.py
+++ twitter/Candidates.py
@@ -1,10 +1,10 @@
 import Candidate;
 
 def initializeCandidates():
-    candict = {'Rick Santorum': ['Santorum'],
-               'Ron Paul': ['Ron Paul'],
-               'Newt Gingrich': ['Newt'],
-               'Mitt Romney': ['Romney']
+    candict = {'Rick Santorum': ['Santorum', '#teamsantorum', '#santorum'],
+               'Ron Paul': ['Ron Paul', '#ronpaul2012'],
+               'Newt Gingrich': ['Newt Gingrich', '#withnewt'],
+               'Mitt Romney': ['Romney', '#romney', '#mitt2012', 'Mitt Romney', '#mittromney']
                 }
     candidates = list()
     for name in candict.keys():
