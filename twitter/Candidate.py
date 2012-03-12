a0ef3cbb6e5826807352d84b74988a7ab18476fb
Alex Madjar <madjara13@gmail.com>
2012-03-12 14:53:11 -0500

    add basic candidates

--- twitter/Candidate.py
+++ twitter/Candidate.py
@@ -1,3 +1,9 @@
+def makeCandidate(name, keywords):
+	ret = Candidate();
+	ret.keywords = keywords;
+	ret.name = name;
+	return ret;
+
 class Candidate:
     def __init__(self):
     	keywords = [];
--- twitter/Candidates.py
+++ twitter/Candidates.py
@@ -1,2 +1,14 @@
-candidates = list()
+import Candidate;
 
+def initializeCandidates():
+    candict = {'Rick Santorum': ['Santorum'],
+               'Ron Paul': ['Ron Paul'],
+               'Newt Gingrich': ['Newt'],
+               'Mitt Romney': ['Romney']
+                }
+    candidates = list()
+    for name in candict.keys():
+        candidates.append(Candidate.makeCandidate(name, candict[name]))
+    return candidates
+
+candidates = initializeCandidates()
