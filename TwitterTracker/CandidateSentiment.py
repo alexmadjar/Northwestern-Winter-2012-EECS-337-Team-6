fa1bf0f56cbff693591411cc132ee77a1dcc0fbd
Nathan Harner <nateharner01@Nathans-MacBook-Pro.local>
2012-03-15 02:38:37 -0500

    Word counting

--- TwitterTracker/CandidateSentiment.py
+++ TwitterTracker/CandidateSentiment.py
@@ -8,13 +8,29 @@ class CandidateSentiment:
             tweetList = []
             
             #Dictionary that maps a word to how often it occurs
+            wordOccurences = {}
             positiveWords = {}
             negativeWords = {}
             for tweet in info:
-                t = TwitterQuery.Tweet() 
+                t = TwitterQuery.Tweet()
                 t.hits = tweet['hits']
                 t.author = tweet['trackback_author_nick']
                 t.content = tweet['content']
                 t.printTweet()
                 tweetList.append(t);
+            for tweet in tweetList:
+                words = tweet.content.lower().split()
+                #Add the words to the list
+                for word in words:
+                    if(wordOccurences.has_key(word)):
+                        wordOccurences[word] = wordOccurences[word] + 1
+                    else:
+                        wordOccurences[word] = 1
+                        
+            for key in wordOccurences:
+                print ("Word: " + key + ' Occurences: ' + str(wordOccurences[word]))
+                
+ 
+c = CandidateSentiment()
+c.run()           
         
