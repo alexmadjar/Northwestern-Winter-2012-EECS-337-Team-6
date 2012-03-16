153b07bdd85e5d0f1f095dfaecfefb866336d8b5
Nathan Harner <nateharner01@Nathans-MacBook-Pro.local>
2012-03-15 21:37:22 -0500

    more filter work

--- TwitterTracker/CandidateSentiment.py
+++ TwitterTracker/CandidateSentiment.py
@@ -4,15 +4,57 @@ class CandidateSentiment:
     def __init__(self):
         #Dictionaries for the naive bayesian filter
         self.positiveWords = eval(open("positiveWords.txt").read())[0]
-        self.numPositiveWords = eval(open("positiveWords.txt").read())[1]
+        self.numPositiveSamples = eval(open("positiveWords.txt").read())[1]
         self.negativeWords = eval(open("negativeWords.txt").read())[0]
-        self.numNegativeWords = eval(open("positiveWords.txt").read())[1]
+        self.numNegativeSamples = eval(open("positiveWords.txt").read())[1]
         
         self.candidates = ['Mitt Romney', 'Rick Santorum', 'Newt Gingrich']
         
-    def train(self, tweetCount):
+    def analyzeSentiment(self, tweetCount):
+        #First, normalize the positiveWords and negativeWords dictionaries
+        for key in self.positiveWords:
+            self.positiveWords[key] = self.positiveWords[key]/self.numPositiveSamples
         
+        for key in self.negativeWords:
+            self.negativeWords[key] = self.negativeWords[key]/self.numNegativeSamples
         
+        #Run Naive Bayes Filter on many tweets
+        for candidate in self.candidates:
+            info = TwitterQuery.search(candidate)
+            #Tweets is a list of dictionaries, where each dictionary is a tweet. The keys are the different parts of the tweet
+            tweetList = []
+            
+            #Dictionary that maps a word to how often it occurs
+            wordOccurences = {}
+                       
+            for tweet in info:
+                t = TwitterQuery.Tweet()
+                t.hits = tweet['hits']
+                t.author = tweet['trackback_author_nick']
+                t.content = tweet['content']
+                t.printTweet()
+                tweetList.append(t);
+                
+            for tweet in tweetList:
+                self.tweetSentiment(tweet) 
+        
+    def tweetSentiment(self, tweet):
+        word_list = tweet.content.lower().split()
+        p_positiveTweet = self.numPositiveSamples/self.numNegativeSamples
+        p_negativeTweet = 1 - p_positiveTweet
+        
+        p_positiveGivenWord = p_positiveTweet
+        p_negativeGivenWord = p_negativeTweet
+        for word in word_list: 
+            p_wordGivenPositive = self.positiveWords[word]
+            p_wordGivenNegative = self.negativeWords[word]
+            
+            p_positiveGivenWord = p_positiveGivenWord * p_wordGivenNegative
+            p_negativeGivenWord = p_negativeGivenWord * p_wordGivenPositive
+            
+            
+        #Print positive probability and negative probability
+        print ('Positive: ' + str(p_positiveGivenWord) + ' Negative: ' + str (p_negativeGivenWord) + '\n' + tweet.content)
         
     def train(self):
         for candidate in self.candidates:
@@ -32,34 +74,26 @@ class CandidateSentiment:
                 tweetList.append(t);
                 
             for tweet in tweetList:                   
-                #Print the tweet, and ask the user to rate if this statement has a positive, negative, or neutral sentiment        
+                #Print the tweet, and ask the user to rate if this statement has a positive or negative sentiment        
                 tweet.printTweet();
-                rated_sentiment = float(raw_input('Tweet Sentiment ( -1 to 1: '))
+                rated_sentiment = float(raw_input('Tweet Sentiment: P or N'))
                 
                 word_list = tweet.content.lower().split()
                 
                 for word in word_list:
-                    #Each word in this tweet is weighted by the rated_sentiment 
                     
-                    if(rated_sentiment >= 0):
+                    if(rated_sentiment == 'P'):
                         if(self.positiveWords.has_key(word)):
-                            self.positiveWords[word] = self.positiveWords[word] + rated_sentiment
+                            self.positiveWords[word] = self.positiveWords[word] + 1
                         else:
                             self.positiveWords[word] = rated_sentiment
-                        self.numPositiveWords = self.numPositiveWords + 1
+                        self.numPositiveSamples = self.numPositiveSamples + 1
                     else:
                         if(self.negativeWords.has_key(word)):
-                            self.negativeWords[word] = self.negativeWords[word] + rated_sentiment
+                            self.negativeWords[word] = self.negativeWords[word] + 1
                         else:
                             self.negativeWords[word] = rated_sentiment
-                        self.numNegativeWords = self.numPositiveWords + 1
-                            
-                    
-                        
-                                     
-                for key in wordOccurences:
-                    
-                #print ("Word: " + key + ' Occurences: ' + str(wordOccurences[key]))
+                        self.numNegativeSamples = self.numNegativeSamples + 1
             
 c = CandidateSentiment()
 c.run()           
